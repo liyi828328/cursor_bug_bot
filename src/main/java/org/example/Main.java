d52a81a3d722a44d0560e3c2d4118b466902d63a
yi.li <yi.li@yulore.com>
2025-12-23 18:23:59 +0800

    测试除0bug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 import cn.hutool.crypto.digest.DigestUtil;
 
 /**
@@ -21,5 +24,15 @@ public class Main {
         System.out.println("Hello, World!");
         String tel = "13800138000";
         System.out.println(DigestUtil.md5Hex(tel));
+
+        List<String> list = Arrays.asList("13800138000", "13800138001", "13800138002");
+        for (String s : list) {
+            System.out.println(DigestUtil.md5Hex(s));
+            if (s.equals("13800138000")) {
+                int a = 100/0;
+                System.out.println(a);
+            }
+        }
+        System.out.println("end");
     }
 }
