3c7510312a5881211212fa418a04fee3b3355a66
yi.li <yi.li@yulore.com>
2025-12-23 18:06:21 +0800

    测试bug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,5 +21,11 @@ public class Main {
         System.out.println("Hello, World!");
         String tel = "13800138000";
         System.out.println(DigestUtil.md5Hex(tel));
+        if (tel.equals("13800138000")) {
+            int a = 100/0;
+            System.out.println(a);
+        } else {
+            System.out.println("tel is not 13800138000");
+        }
     }
 }
